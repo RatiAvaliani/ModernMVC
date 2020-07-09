25cc4db126f2f68a907fc4247827b00828644e4b
Rati Avaliani <ratijugeli@gmail.com>
2020-07-09 21:02:13 +0400

    Update install.sql

--- install.sql
+++ install.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 22, 2020 at 03:10 AM
+-- Generation Time: Jul 09, 2020 at 07:01 PM
 -- Server version: 10.4.8-MariaDB
 -- PHP Version: 7.3.11
 
@@ -25,16 +25,122 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `langs`
+--
+
+CREATE TABLE `langs` (
+  `id` int(11) NOT NULL,
+  `lang_title` varchar(255) NOT NULL,
+  `status` int(11) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `langs`
+--
+
+INSERT INTO `langs` (`id`, `lang_title`, `status`) VALUES
+(1, 'ka', 1),
+(2, 'en', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `lang_parameter`
+--
+
+CREATE TABLE `lang_parameter` (
+  `id` int(11) NOT NULL,
+  `vv_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `privilege` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `privilege`, `username`, `password`) VALUES
+(1, '1', 'admin', '$2y$10$gaKNJhIL.AVt2kBnYMP1uet9jiyHKaslfYjYVOCxNxO0.P/KSPtky');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `virtualvariables`
 --
 
 CREATE TABLE `virtualvariables` (
   `id` int(11) NOT NULL,
   `type` int(11) NOT NULL,
-  `lang` int(11) DEFAULT NULL,
+  `lang` int(11) NOT NULL DEFAULT 0,
   `name` varchar(255) NOT NULL,
-  `content` varchar(255) NOT NULL
+  `content` text NOT NULL,
+  `status` int(11) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `virtualvariables`
+--
+
+INSERT INTO `virtualvariables` (`id`, `type`, `lang`, `name`, `content`, `status`) VALUES
+(1, 1, 1, 'menu', 'მენუ', 1),
+(2, 1, 2, 'menu', 'menu', 1),
+(3, 1, 1, 'menus', 'მენუs', 1),
+(4, 1, 2, 'menua', 'menua', 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `langs`
+--
+ALTER TABLE `langs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `virtualvariables`
+--
+ALTER TABLE `virtualvariables`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `langs`
+--
+ALTER TABLE `langs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `virtualvariables`
+--
+ALTER TABLE `virtualvariables`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
